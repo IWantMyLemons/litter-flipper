50a14e9b8d0d9af22518eb1d1a437bf9e4d9118e
Jason <jason.ang2005@gmail.com>
2024-07-25 23:14:38 +0700

    implemented igloo healing fish(sorta, we dont have healed fish yet)

---
+++ Assets/Igloo/Scripts/FishHealing.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FishHealing : MonoBehaviour
+{
+    [Tooltip("time it takes to heal in seconds")]
+    public float healingTime;
+    [Tooltip("fish that will be thrown out once healed(they don't have insurance)")]
+    public GameObject healedFish;
+    [Tooltip("where new fish spawn")]
+    public Transform spawnPoint;
+
+    static readonly int capacity = 1;
+
+    readonly Queue<float> fishQueue = new(capacity);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fishQueue.Count > 0 && Time.time > fishQueue.Peek())
+        {
+            fishQueue.Dequeue();
+            Instantiate(healedFish, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name.ContainsInsensitive("fish")) // honestly a dumb way of checking for fish
+        {
+            if (fishQueue.Count < capacity)
+            {
+                Destroy(collider.gameObject);
+                fishQueue.Enqueue(Time.time + healingTime);
+            }
+            else
+            {
+                collider.GetComponent<Rigidbody2D>().velocity *= -1;
+            }
+        }
+    }
+
+}
