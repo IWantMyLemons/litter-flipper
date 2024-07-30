c315c1a49c76dbf856fde5138ddca8f757239ce5
Jason <jason.ang2005@gmail.com>
2024-07-30 22:48:18 +0700

    Implemented fish variants and animations

--- Assets/Igloo/Scripts/FishHealing.cs
+++ Assets/Igloo/Scripts/FishHealing.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,27 +10,44 @@ public class FishHealing : MonoBehaviour
     [Tooltip("time it takes to heal in seconds")]
     public float healingTime;
     [Tooltip("fish that will be thrown out once healed(they don't have insurance)")]
-    public GameObject healedFish;
+    public GameObject healedFish1;
+    [Tooltip("the other one")]
+    public GameObject healedFish2;
     [Tooltip("where new fish spawn")]
     public Transform spawnPoint;
 
     static readonly int capacity = 3;
 
-    readonly Queue<float> fishQueue = new(capacity);
+    struct FishPatient
+    {
+        public FishPatient(float finished, int variant)
+        {
+            this.finished = finished;
+            this.variant = variant;
+        }
+        public float finished;
+        public int variant;
+    }
+
+    readonly Queue<FishPatient> fishQueue = new(capacity);
 
     // Update is called once per frame
     void Update()
     {
-        if (fishQueue.Count > 0 && Time.time > fishQueue.Peek())
+        if (fishQueue.Count > 0 && Time.time > fishQueue.Peek().finished)
         {
-            fishQueue.Dequeue();
-            GameObject newFish = Instantiate(healedFish, spawnPoint.position, spawnPoint.rotation);
-            newFish.name = "Healed Fish";
-            newFish.GetComponentInChildren<Animator>().SetBool("is_healed", true);
-
-            TrashItem trashItem = newFish.AddComponent<TrashItem>();
-            trashItem.trashCategories = new string[1];
-            trashItem.trashCategories[0] = "sea";
+            switch (fishQueue.Dequeue().variant)
+            {
+                case 1:
+                    Instantiate(healedFish1, spawnPoint.position, spawnPoint.rotation);
+                    break;
+                case 2:
+                    Instantiate(healedFish2, spawnPoint.position, spawnPoint.rotation);
+                    break;
+                default:
+                    Debug.LogWarning("Invalid fish variant in hospital");
+                    break;
+            }
         }
     }
 
@@ -41,7 +59,9 @@ public class FishHealing : MonoBehaviour
             && collider.GetComponent<FishDespawning>().is_alive)
             {
                 Destroy(collider.gameObject);
-                fishQueue.Enqueue(Time.time + healingTime);
+
+                int variant = 1;
+                fishQueue.Enqueue(new FishPatient(Time.time + healingTime, variant));
             }
             else
             {
