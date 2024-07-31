85fa41f546f512a305d4af6cc48b6c99dc287294
Jason <jason.ang2005@gmail.com>
2024-07-31 21:34:52 +0700

    implemented spawner

---
+++ Assets/Spawner/SpawnObjects.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [System.Serializable]
+    public class SpawnAction
+    {
+        public float delay;
+        public int count;
+    }
+
+    [Tooltip("Object(s) to spawn")]
+    public GameObject[] toSpawn;
+
+    [Tooltip("When to spawn")]
+    public SpawnAction[] schedule;
+
+    PolygonCollider2D spawnArea;
+    int currSchedule = 0;
+    float timePassed = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spawnArea = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed > schedule[currSchedule].delay)
+        {
+            timePassed -= schedule[currSchedule].delay;
+            if (schedule[currSchedule].count <= 0)
+            {
+                currSchedule = (currSchedule + 1) % schedule.Length;
+            }
+            else
+            {
+                SpawnObject();
+                schedule[currSchedule].count--;
+            }
+        }
+    }
+
+    void SpawnObject()
+    {
+        Vector3 minBounds = spawnArea.bounds.min;
+        Vector3 maxBounds = spawnArea.bounds.max;
+
+        // Currently objects are spawned by choosing a random point and checking whether its inside
+        // this should still be performant since it usually lands after 1-2 iterations
+        Vector2 randomPoint;
+        int iterations = 0;
+        do
+        {
+            iterations++;
+            randomPoint.x = Mathf.Lerp(minBounds.x, maxBounds.x, Random.value);
+            randomPoint.y = Mathf.Lerp(minBounds.y, maxBounds.y, Random.value);
+        } while (!spawnArea.OverlapPoint(randomPoint));
+        Instantiate(toSpawn[Random.Range(0, toSpawn.Length)], randomPoint, Quaternion.identity);
+    }
+}
